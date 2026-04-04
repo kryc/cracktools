320b9fd6e993e60341a60b52e20f20bb31dfad2c
Gareth Evans <gazz.sf@gmail.com>
2026-04-04 10:59:25 +0100

    Add randstring

---
+++ src/RandStringMain.cpp
@@ -0,0 +1,117 @@
+//
+//  RandStringMain.cpp
+//  RandString
+//
+//  Created by Kryc on 04/04/2026.
+//  Copyright © 2026 Kryc. All rights reserved.
+//
+
+#include <fstream>
+#include <iostream>
+#include <string_view>
+
+#include "Random.hpp"
+#include "UnsafeBuffer.hpp"
+#include "Util.hpp"
+#include "WordGenerator.hpp"
+
+#define ARGCHECK() \
+    if (argc <= i) \
+    { \
+        std::cerr << "No value specified for " << arg << std::endl; \
+        return 1; \
+    }
+
+const std::string HELP_STRING = R"(
+Usage: randstring [options] <charset>
+Options:
+  --min <value>       Set the minimum password length.
+  --max <value>       Set the maximum password length.
+  --length <value>    Set the exact password length.
+  --charset <string>  Set the character set to use.
+  --help              Display this help message.
+)";
+
+int main(
+    const int argc,
+    const char* argv[])
+{
+    // Parse the command line arguments
+    auto args = cracktools::ParseArgv(argv, argc);
+
+    size_t min = 8;
+    size_t max = 32;
+    std::string_view charset = ALPHANUMERIC;
+    std::string_view additional;
+
+    for (int i = 1; i < argc; i++)
+	{
+        const std::string_view arg = args[i];
+        if (arg == "--min" || arg == "-m")
+        {
+            ARGCHECK();
+            min = Util::ParseNumber<size_t>(args[++i]);
+        }
+        else if (arg == "--max" || arg == "-M")
+        {
+            ARGCHECK();
+            max = Util::ParseNumber<size_t>(args[++i]);
+        }
+        else if (arg == "--length" || arg == "-l")
+        {
+            ARGCHECK();
+            max = Util::ParseNumber<size_t>(args[++i]);
+            min = max;
+        }
+        else if (arg == "--charset" || arg == "-c")
+        {
+            ARGCHECK();
+            charset = ParseCharset(args[++i]);
+        }
+        else if (arg == "--additional" || arg == "-a")
+        {
+            ARGCHECK();
+            additional = ParseCharset(args[++i]);
+        }
+        else if (arg == "--help" || arg == "-h")
+        {
+            std::cout << HELP_STRING;
+            return 0;
+        }
+        else
+        {
+            std::cerr << "Unknown option " << arg << std::endl;
+            return 1;
+        }
+    }
+
+    // Create a WordGenerator instance
+    std::string usedCharset = std::string(charset);
+    if (!additional.empty())
+    {
+        // Append additional characters to the charset
+        usedCharset += std::string(additional);
+    }
+    WordGenerator generator(usedCharset);
+
+    // Create a PRNG based on the time
+    MiniPRNG64 prng(std::chrono::steady_clock::now().time_since_epoch().count());
+
+    // If min != max, we need to generate a random length
+    size_t length = (min == max) ? min : 0;
+    if (length == 0)
+    {
+        uint64_t diff = max - min + 1;
+        length = prng.Next() % diff + min;
+    }
+
+    // Generate a random string of the given length
+    for (size_t i = 0; i < length; i++)
+    {
+        char c = charset[prng.Next() % charset.size()];
+        std::cout << c;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ src/Random.hpp
@@ -0,0 +1,78 @@
+//
+//  Random.hpp
+//  CrackTools
+//
+//  Created by Kryc on 04/04/2026.
+//  Copyright © 2026 Kryc. All rights reserved.
+//
+
+#ifndef RANDOM_HPP
+#define RANDOM_HPP
+
+#include <bit>
+#include <cstdint>
+
+constexpr uint32_t kMiniPRNGDefaultSeed = 0xd34db33f;
+
+template <typename T>
+class MiniPRNG
+{
+public:
+    MiniPRNG(T Seed = kMiniPRNGDefaultSeed)
+    {
+        a = 0xf1ea2eed;
+        b = c = d = Seed;
+        for (int i = 0; i < 20; ++i) {
+            Next();
+        }
+    }
+    inline const T Next(void)
+    {
+        T e;
+        if constexpr (std::is_same<T, uint64_t>::value) {
+            e = a - std::rotl(b, 7);
+            a = b ^ std::rotl(c, 13);
+            b = c + std::rotl(d, 37);
+        } else if constexpr (std::is_same<T, uint32_t>::value) {
+            e = a - std::rotl(b, 32);
+            a = b ^ std::rotl(c, 16);
+            b = c + std::rotl(d, 11);
+        }
+        d = e + a;
+        c = d + e;
+        d = e + a;
+        return d;
+    }
+    inline const T NextEven(void)
+    {
+        return Next() & ~1ULL;
+    }
+private:
+    T a, b, c, d;
+};
+
+// Declare templates specially for 32-bit and 64-bit PRNGs
+using MiniPRNG32 = MiniPRNG<uint32_t>;
+using MiniPRNG64 = MiniPRNG<uint64_t>;
+
+template <typename T>
+inline const T
+RandomInRange(
+    MiniPRNG64& Prng,
+    const T Upper
+)
+{
+    if constexpr (std::is_same<T, uint32_t>::value ||
+        std::is_same<T, uint64_t>::value) {
+        return Prng.Next() % Upper;
+    } else if constexpr (std::is_same<T, __uint128_t>::value) {
+        __uint128_t result = Prng.Next();
+        result <<= 64;
+        result |= Prng.Next();
+        return result % Upper;
+    } else {
+        static_assert(sizeof(T) == 0, "RandomInRange only supports uint32_t and uint64_t types.");
+    }
+}
+
+#endif // RANDOM_HPP
